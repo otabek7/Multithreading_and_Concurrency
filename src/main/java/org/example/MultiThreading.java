79dc6202626f2df993a62d7fcea5f2fe57873e40
Otabek <mavlonovo@tamu.edu>
2024-09-27 23:13:32 -0500

    testing multithreading

---
+++ src/main/java/org/example/MultiThreading.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class MultiThreading extends Thread{
+    @Override
+    public void run(){
+        for (int i = 0; i<5; i++){
+            System.out.println(i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
